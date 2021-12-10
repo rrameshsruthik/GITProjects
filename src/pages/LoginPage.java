df169bbeb314b11877a2e6895944b5810b8c98f9
unknown <rrameshsruthik@gmail.com>
2021-12-10 14:45:44 +0530

    Login code is added

---
+++ src/pages/LoginPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+public class LoginPage {
+	
+	public LoginPage() {
+	
+		System.out.println("Credentials");
+		
+		System.out.println("Username"+ "admin");
+		
+		System.out.println("Password"+"admin@123");
+		
+		System.out.println("Login Successfully");
+	}
+
+}
